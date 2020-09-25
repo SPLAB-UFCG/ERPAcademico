ac976b9dd4d57838f14b130d495ad28f292a9f6d
Luiz Bonfim <luiz.bonfim.neto@ccc.ufcg.edu.br>
2020-09-25 01:18:39 -0300

    Fix do Obj/Json de professores

--- experimental/luiz/PDF-PARSE/utils/joinInfo.js
+++ experimental/luiz/PDF-PARSE/utils/joinInfo.js
@@ -4,7 +4,7 @@ function joinClassesWithSchedule(classes, schedule, teachers) {
     if (classes.length === teachers.length && classes.length === schedule.length) {
         for(let i = 0; i < classes.length; i++) {
             // Criando um array com as disciplinas e seus respectivos horários
-            classesRelation.push([classes[i], schedule[i]])
+            classesRelation.push([teachers[i] ,classes[i], schedule[i]])
         }
     }
 
@@ -15,10 +15,18 @@ function createObjectTeachersClasses(teachers, classesRelation) {
     const teachersObj = {};
     
     for (let i = 0; i < teachers.length; i++) {
-        //Criando um objeto com a key sendo o nome dos professores da disciplina, e o value
-        // sendo a disciplina e o horário da mesma.
-        teachersObj[teachers[i]] = classesRelation[i];       
+        
+        teachersObj[classesRelation[i][0]] = []       
     };
+
+    for (let i = 0; i < classesRelation.length; i++) {
+        for (const teacherName in teachersObj) {
+                if(teacherName === classesRelation[i][0]) {
+                    teachersObj[teacherName].push([classesRelation[i][1], classesRelation[i][2]]);
+                };
+            };
+        };
+    
 
     return teachersObj;
 }
